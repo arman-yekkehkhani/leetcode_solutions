b079a80914d75e405b86bfb1cac97b5fef2c95b2
arman-yekkehkhani <arman.yekkehkhani@gmail.com>
2024-07-04 11:31:17 +0330

    lc(110): balanced Binary Tree

---
+++ 110-Balanced_Binary_Tree/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Author: Arman Yekkehkhani
+ * Runtime: 1ms (beats 100%)
+ * Memory: 44.24 MB (beats 44.54%)
+ * tags: hash
+ */
+
+class Solution {
+    boolean isb = true;
+
+    public boolean isBalanced(TreeNode root) {
+        height(root);
+        return isb;
+    }
+
+    int height(TreeNode n) {
+        if (n == null) {
+            return 0;
+        }
+
+        int l = height(n.left);
+        int r = height(n.right);
+        if (Math.abs(l - r) > 1) {
+            isb = false;
+        }
+        return 1 + Math.max(l, r);
+    }
+
+}
