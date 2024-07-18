aa1471bf3eb294713bcf5b8bd8e626472993d56c
arman-yekkehkhani <arman.yekkehkhani@gmail.com>
2024-07-18 14:59:47 +0330

    lc(701): insertIntoBST

---
+++ 701-Insert_into_a_Binary_Search_Tree/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Author: Arman Yekkehkhani
+ * Runtime: 0ms (beats 100%)
+ * Memory: 45.15 MB (beats 51.95%)
+ * tags: binary search tree, bst
+ */
+
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        if (val < root.val) {
+            if (root.left == null) {
+                root.left = new TreeNode(val);
+            } else {
+                insertIntoBST(root.left, val);
+            }
+        } else {
+            if (root.right == null) {
+                root.right = new TreeNode(val);
+            } else {
+                insertIntoBST(root.right, val);
+            }
+        }
+        return root;
+    }
+}
