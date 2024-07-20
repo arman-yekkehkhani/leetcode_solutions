ad9563fb5cd1183b244970a88f3ec4ff3b73019e
arman-yekkehkhani <arman.yekkehkhani@gmail.com>
2024-07-20 12:43:47 +0330

    lc(814): pruneTree

---
+++ 814-Binary_Tree_Pruning/Solution.java
@@ -0,0 +1,33 @@
+/**
+ * Author: Arman Yekkehkhani
+ * Runtime: 0 ms (beats 100.00 %)
+ * Memory: 41.02 MB (beats 50.73 %)
+ * tags:
+ */
+
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if (noOne(root)) {
+            return null;
+        }
+        return root;
+    }
+
+    boolean noOne(TreeNode n) {
+        if (n == null) {
+            return true;
+        }
+
+        var l = noOne(n.left);
+        if (l) {
+            n.left = null;
+        }
+
+        var r = noOne(n.right);
+        if (r) {
+            n.right = null;
+        }
+
+        return r && l && n.val == 0;
+    }
+}
